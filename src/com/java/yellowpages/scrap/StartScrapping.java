b4c04a184474a5074f278e3d2b5223596fcb24d6
Rahul Sahay <rahul.sahay@voya.com>
2018-02-11 23:18:28 -0500

    updates

--- src/com/java/yellowpages/scrap/StartScrapping.java
+++ src/com/java/yellowpages/scrap/StartScrapping.java
@@ -5,7 +5,10 @@ package com.java.yellowpages.scrap;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,79 +22,103 @@ import org.jsoup.select.Elements;
  */
 public class StartScrapping extends AbstractScrapper {
 	
-	
 	private static final int START_PAGE = 1;
-	private static final int TOTAL_PAGES = 2;
-	private static final String FOLDER_NAME = "3d";
+	private static final String PROCESS_FOLDER = "colors";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		String searchString;  
-//
-//		System.out.println("Enter Search Name : ");
-//		searchString = br.readLine();
-		
 		disableSSLCertCheck();
 		
-		File file = new File("./images/" + FOLDER_NAME);
-        if (!file.exists()) {
-            if (file.mkdir()) {
-                System.out.println("Directory is created!");
-            } else {
-                System.out.println("Failed to create directory!");
-            }
-        }
+		Map<String, String> folderURLNameMap = new LinkedHashMap<String, String>();
+		Document docMain = Jsoup.connect("http://eskipaper.com").get();
+		System.out.println("==================================================================");
+		System.out.println(docMain.title());
+		System.out.println("==================================================================");
 		
-		
-        List<String> imageURLs = new ArrayList<String>();
-        
-		for(int i=START_PAGE; i <= TOTAL_PAGES; i++) {
-			Document doc = Jsoup.connect("http://eskipaper.com/" + FOLDER_NAME + "/" + i + "/").get();
-			System.out.println("==================================================================");
-			System.out.println(doc.title());
-			System.out.println("==================================================================");
-			Elements newsHeadlines = doc.select(".category-list-item");
+		Elements folderListItems = docMain.select(".toplevel ul li");
+		for (Element folderElem : folderListItems) {
+			String folderURL = folderElem.select("a").get(0).absUrl("href");
+			String folderName = folderElem.select("a").get(0).text();
 			
-			//Map<String, String> advocateLinkMap = new HashMap<String, String>();
-			for (Element headline : newsHeadlines) {
-				//System.out.println(headline.html() + " : " + headline.absUrl("href"));
-				//advocateLinkMap.put(headline.html(), headline.absUrl("href"));
-				
-//				if(headline.html().contains(searchString)) {
-					//System.out.println("Got this image link ---> " + headline.absUrl("href"));
-				
-					Elements linkTagElem = headline.select("a");
-					Document newDoc = Jsoup.connect(linkTagElem.get(0).absUrl("href")).get();
-					Elements rowElements = newDoc.select(".imgdata .download-img");
-					
-					String resolution = rowElements.get(0).text();
-					String url = rowElements.get(0).absUrl("href");
-					
-					System.out.println("Resolution : " + resolution + " ========= Image Link : " +  url);
-					imageURLs.add(url);
-					
-//					for (Element row : rowElements) {
-//						Elements e = row.select("span");
-//						String resolution = e.text();
-//						if(resolution != null && resolution.indexOf("Original Resolution:") != -1) {
-//							Elements originalAnchors = e.select("a");
-//							for (Element a : originalAnchors) {
-//								System.out.println("Resolution : " + resolution + " ========= Image Link : " +  a.absUrl("href"));
-//								
-//								imageURLs.add(a.absUrl("href"));
-//								//saveImage(a.absUrl("href"));
-//							}
-//						}
-//						//System.out.println(e.text());
-//					}
-//				}
-			}
+			folderURLNameMap.put(folderName, folderURL);
 		}
 		
-		bulkSaveImage(imageURLs, FOLDER_NAME);
+		for (Map.Entry<String, String> entry : folderURLNameMap.entrySet()) {
+		    String folderName = entry.getKey();
+		    String folderURL = entry.getValue();
+		    
+		    if(!PROCESS_FOLDER.equals("") && !PROCESS_FOLDER.equalsIgnoreCase(folderName)) {
+		    	continue;
+		    }
+		    
+		    System.out.println("==================================================================");
+		    System.out.println("Processing Folder - " + folderName + " : START");
+			System.out.println("==================================================================");
+		    
+		    File file = new File("./images/" + folderName);
+	        if (!file.exists()) {
+	            if (file.mkdir()) {
+	                System.out.println("Directory is created!");
+	            } else {
+	                System.out.println("Failed to create directory!");
+	            }
+	        }
+	        
+	        try {
+		        int TOTAL_PAGES = 0;
+		        Document parentDoc = Jsoup.connect(folderURL).get();
+		        Elements paginationElems = parentDoc.select(".pagination2 li");
+		        for (Element p : paginationElems) {
+		        	if(p.select("a").size() == 0)
+		        		continue;
+		        	
+		        	Element aTag = p.select("a").get(0);
+		        	String t = aTag.select("span").get(0).text();
+		        	if(t.equalsIgnoreCase("End")) {
+		        		String link = aTag.absUrl("href");
+		        		TOTAL_PAGES = Integer.valueOf(link.substring(folderURL.length(), link.length() - 1));
+		        		break;
+		        	}
+		        }
+		        
+		        List<String> imageURLs = new ArrayList<String>();
+				for(int i=START_PAGE; i <= TOTAL_PAGES; i++) {
+					Document doc = Jsoup.connect(folderURL + i + "/").get();
+					
+					System.out.println("==================================================================");
+					System.out.println(doc.title() + " Page - " + i);
+					System.out.println("==================================================================");
+					Elements newsHeadlines = doc.select(".category-list-item");
+					
+					for (Element headline : newsHeadlines) {
+							Elements linkTagElem = headline.select("a");
+							Document newDoc = Jsoup.connect(linkTagElem.get(0).absUrl("href")).get();
+							Elements rowElements = newDoc.select(".imgdata .download-img");
+							
+							String resolution = rowElements.get(0).text();
+							String url = rowElements.get(0).absUrl("href");
+							
+							System.out.println("Resolution : " + resolution + " ========= Image Link : " +  url);
+							imageURLs.add(url);
+					}
+				}
+				
+				bulkSaveImage(imageURLs, folderName);
+				
+				System.out.println("==================================================================");
+				System.out.println("Processing Folder - " + folderName + " : END");
+				System.out.println("==================================================================");
+	        }
+	        catch(Exception e) {
+	        	System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	        	System.out.println("Error while processing Folder : " + folderName);
+	        	System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	        	e.printStackTrace();
+	        }
+		    
+		}
 	}
 	
 }
